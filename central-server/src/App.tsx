2a31daf1e1dd4e6235673971a0f081dcc6164497
ErickGiffoni <giffoni.erick@gmail.com>
2022-05-03 17:59:26 -0300

    tentando conectar com o mosquito\!

--- central-server/src/App.tsx
+++ central-server/src/App.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Message } from 'paho-mqtt';
+
+import mqttClient from './clients/mqtt';
 
 function App() {
+
+  const devicesTopic = "fse2021/170141161/dispositivos/";
+
+  // using paho-mqtt lib
+
+  mqttClient.connect({onSuccess: () => {
+    console.log("onConnect");
+    mqttClient.subscribe(devicesTopic);
+
+    let message = new Message("Im the central server !");
+    message.destinationName = devicesTopic;
+    mqttClient.send(message);
+
+    return;
+  }});
+
+  mqttClient.onMessageArrived = (message) => {
+    console.log("onMessageArrived: "+message.payloadString);
+    return;
+  };
+
+  // using mqtt lib
+  // mqttClient.on('connect', () => {
+  //   mqttClient.subscribe(devicesTopic, (err, granted) => {
+  //     if(err){
+  //       console.log('MQTT ERROR: couldnt subscribe to ' + devicesTopic);
+  //       return;
+  //     }
+
+  //     console.log('MQTT: successfully subscribed to ' + devicesTopic);
+
+  //     mqttClient.publish(devicesTopic, 'Im the central server !');
+
+  //     return;
+  //   })
+  // });
+
+  // mqttClient.on('message', (topic, message) => {
+  //   console.log('MQTT: new message -> ', message.toString());
+  //   console.log('\tfrom topic ', topic.toString());
+
+  //   return;
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
