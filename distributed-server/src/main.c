63a0afd46264a284f29828280c91a40608d17e8c
Joao Pedro <jps12carvalho@gmail.com>
2022-05-04 23:09:08 -0300

    feat: Adding http client to test connection

--- distributed-server/src/main.c
+++ distributed-server/src/main.c
@@ -1,9 +1,38 @@
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_tls.h"
+
 #include "wifi.h"
 #include "flash.h"
+#include "http_client.h"
 #include "mqtt.h"
+
+xSemaphoreHandle conexaoWifiSemaphore;
+
+void RealizaHTTPRequest(void *params){
+    while (true){
+        if (xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY)){
+            ESP_LOGI("Main Task", "Realiza HTTP Request");
+            mqtt_start();
+        }
+    }
+}
 
 void app_main() {
     Flash_init();
+
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+
     wifi_config();
-    mqtt_start();
+
+    xTaskCreate(&RealizaHTTPRequest, "Processa HTTP", 4096, NULL, 1,NULL);
+    
+
 }
